0d044a96c5d2ad8986aa493fe9df88b3613da051
root <mituhashitakashi@gmail.com>
2020-01-13 07:26:01 +0000

    add assessment feature per title

--- src/accumulators.py
+++ src/accumulators.py
@@ -11,6 +11,7 @@ class UserStatsAcc:
         super().__init__()
         self.win_code = win_code
         self.event_code_list = event_code_list
+        self.ass_title_list = [12, 21,  2, 18, 30]
         self.event_feat_name = []
         self.title_feat_name = []
         for title in self.win_code.keys():
@@ -24,16 +25,13 @@ class UserStatsAcc:
             {feat_name: 0 for feat_name in self.event_feat_name}))
         self.title_couter = defaultdict(lambda: Counter(
             {feat_name: 0 for feat_name in self.title_feat_name}))
-        # self.title_event_feat_name += [
-        #     f'title{title}_count' for title in self.win_code.keys()]
-        # self.title_event_feat_name += [
-        #     f'event_code{event_code}_count' for event_code in self.event_code_list]
         self.assessment_acc = AssessmentAcc(win_code)
         self.session_acc = SessionAcc(self.event_feat_name)
         self.user_activities_count = defaultdict(lambda: defaultdict(int))
         self.last_session_id = defaultdict(lambda: None)
         self.last_activity = defaultdict(int)
         self.last_title = defaultdict(lambda: None)
+        self.last_title_acc = defaultdict(lambda: defaultdict(lambda: -1))
         self.last_world = {}
         self.last_time = {}
         self.durations = defaultdict(list)
@@ -43,12 +41,13 @@ class UserStatsAcc:
         ins_id = row['installation_id']
         session_id = row['game_session']
         session_type = row['type']
+        session_title = row['title']
         if session_id != self.last_session_id.get(ins_id):
             self.user_activities_count[ins_id][session_type] += 1
         self.user_activities_count[ins_id]['all_actions_count'] += 1
         self.last_session_id[ins_id] = session_id
         self.last_activity[ins_id] = session_type
-        self.last_title[ins_id] = row['title']
+        self.last_title[ins_id] = session_title
         self.last_world[ins_id] = row['world']
         self.last_time[ins_id] = row['timestamp']
         # update chile accumulators
@@ -56,13 +55,9 @@ class UserStatsAcc:
         self.session_acc.update_acc(row)
         ass_stats = self.assessment_acc.get_stats(row)
         session_stats = self.session_acc.get_stats(row)
-        # print(ins_id, row['game_session'], session_type, row['end_of_game'],
-        #       session_stats['step_count_in_game'], self.is_evaluate_timing)
         if row['end_of_game']:
             self.user_activities_count[ins_id]['game_count'] += 1
-            self.title_couter[ins_id][f'title{row["title"]}_count'] += 1
-            # for feat_name in self.title_event_feat_name:
-            #     self.user_activities_count[ins_id][feat_name] += session_stats[feat_name]
+            self.title_couter[ins_id][f'title{session_title}_count'] += 1
             self.event_code_counter[ins_id].update(
                 self.session_acc.get_event_count(row))
 
@@ -70,13 +65,23 @@ class UserStatsAcc:
             self.session_acc.delete_data(row)
 
         if self.is_evaluate_timing(row, ass_stats):
+            self.user_activities_count[ins_id][f'title{session_title}_evaluate_count'] += 1
+            self.user_activities_count[ins_id][
+                f'title{session_title}_true_attempts_count'] += ass_stats['true_attempts_count']
+            self.user_activities_count[ins_id][
+                f'title{session_title}_false_attempts_count'] += ass_stats['false_attempts_count']
+            self.user_activities_count[ins_id][f'title{session_title}_accumulated_accracy'] += ass_stats['accuracy']
+            self.user_activities_count[ins_id][
+                f'title{session_title}_accumulated_accracy_group'] += ass_stats['accuracy_group']
             self.user_activities_count[ins_id]['evaluate_count'] += 1
             self.user_activities_count[ins_id]['true_attempts_count'] += ass_stats['true_attempts_count']
             self.user_activities_count[ins_id]['false_attempts_count'] += ass_stats['false_attempts_count']
             self.user_activities_count[ins_id]['accumulated_accracy'] += ass_stats['accuracy']
             self.user_activities_count[ins_id]['accumulated_accracy_group'] += ass_stats['accuracy_group']
             accuracy_group = ass_stats['accuracy_group']
+            self.last_title_acc[ins_id][f'title{session_title}_last_acc_group'] = accuracy_group
             self.user_activities_count[ins_id][f'acc_group_{accuracy_group}_count'] += 1
+            self.user_activities_count[ins_id][f'title{session_title}_acc_group_{accuracy_group}_count'] += 1
             self.durations[ins_id].append(session_stats['session_time_length'])
 
     def get_stats(self, row):
@@ -98,20 +103,27 @@ class UserStatsAcc:
             = self.user_activities_count[ins_id]['false_attempts_count']
         evaluate_count = self.user_activities_count[ins_id]['evaluate_count']
         output['ave_accuracy'] = self.user_activities_count[ins_id]['accumulated_accracy'] / \
-            evaluate_count if evaluate_count > 0 else 0
+            evaluate_count if evaluate_count > 0 else -1
         output['ave_accuracy_group'] = self.user_activities_count[ins_id]['accumulated_accracy_group'] / \
-            evaluate_count if evaluate_count > 0 else 0
+            evaluate_count if evaluate_count > 0 else -1
         output['acc_group_0_count'] = self.user_activities_count[ins_id]['acc_group_0_count']
         output['acc_group_1_count'] = self.user_activities_count[ins_id]['acc_group_1_count']
         output['acc_group_2_count'] = self.user_activities_count[ins_id]['acc_group_2_count']
         output['acc_group_3_count'] = self.user_activities_count[ins_id]['acc_group_3_count']
+        for ass_title in self.ass_title_list:
+            title_eval_count = self.user_activities_count[
+                ins_id][f'title{ass_title}_evaluate_count']
+            output[f'title{ass_title}_true_attempts_count'] = self.user_activities_count[ins_id][f'title{ass_title}_true_attempts_count']
+            output[f'title{ass_title}_false_attempts_count'] = self.user_activities_count[ins_id][f'title{ass_title}_false_attempts_count']
+            output[f'title{ass_title}_ave_accracy'] = self.user_activities_count[ins_id][f'title{ass_title}_accumulated_accracy'] / \
+                title_eval_count if title_eval_count > 0 else -1
+            output[f'title{ass_title}_ave_accracy_group'] = self.user_activities_count[ins_id][
+                f'title{ass_title}_accumulated_accracy_group'] / title_eval_count if title_eval_count > 0 else -1
+            output[f'title{ass_title}_last_acc_group'] = self.last_title_acc[ins_id][f'title{ass_title}_last_acc_group']
         if len(self.durations[ins_id]) == 0:
             output['duration_mean'] = 0
         else:
             output['duration_mean'] = np.mean(self.durations[ins_id])
-
-        # for feat_name in self.title_event_feat_name:
-        #     output[feat_name] = self.user_activities_count[ins_id][feat_name]
 
         return output
 
