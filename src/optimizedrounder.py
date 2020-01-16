8e90e100196e57beb77f769924c66bb9ad31f719
root <mituhashitakashi@gmail.com>
2020-01-16 01:21:06 +0000

    add HistBaseRounder

--- src/optimizedrounder.py
+++ src/optimizedrounder.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from functools import partial
 
 import numpy as np
@@ -57,3 +58,43 @@ class OptimizedRounder(object):
         Return the optimized coefficients
         """
         return self.coef_['x']
+
+
+class HistBaseRounder:
+    def __init__(self):
+        super().__init__()
+        self.labels = [0, 1, 2, 3]
+        self.coef = None
+
+    def fit(self, X, y):
+        dist = Counter(y)
+        for k in dist:
+            dist[k] /= len(y)
+
+        acum = 0
+        bound = {}
+        for i in range(len(self.labels) - 1):
+            acum += dist[i]
+            bound[i] = np.percentile(X, acum * 100)
+        self.coef = bound
+
+    def predict(self, X, coef=None):
+        if not coef:
+            coef = self.coef
+        X_p = np.copy(X)
+        for i, pred in enumerate(X_p):
+            if pred < coef[0]:
+                X_p[i] = 0
+            elif pred >= coef[0] and pred < coef[1]:
+                X_p[i] = 1
+            elif pred >= coef[1] and pred < coef[2]:
+                X_p[i] = 2
+            else:
+                X_p[i] = 3
+        return X_p
+
+    def coefficients(self):
+        """
+        Return the optimized coefficients
+        """
+        return self.coef
