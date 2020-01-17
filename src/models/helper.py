198276d7b9642f465ee0762aff73462129eac78e
root <mituhashitakashi@gmail.com>
2020-01-17 11:06:25 +0000

    add xgboost

--- src/models/helper.py
+++ src/models/helper.py
@@ -1,8 +1,10 @@
 from models.model_lgbm import ModelLGBMClassifier, ModelLGBMRegressor
 from models.model_catboost import ModelCatBoostRegressor
+from models.model_xgb import ModelXGBRegressor
 
 MODEL_MAP = {
     'ModelLGBMClassifier': ModelLGBMClassifier,
     'ModelLGBMRegressor': ModelLGBMRegressor,
-    'ModelCatBoostRegressor': ModelCatBoostRegressor
+    'ModelCatBoostRegressor': ModelCatBoostRegressor,
+    'ModelXGBRegressor': ModelXGBRegressor
 }
